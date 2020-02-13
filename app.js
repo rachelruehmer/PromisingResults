160adcf01043010f4bddf922aa68821dc269bc1c
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-02-13 13:47:56 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// slowMath.add(6, 2)
+//     .then(num => {
+//         console.log(num);
+//         return slowMath.multiply(num, 2);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.divide(num, 4);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.subtract(num, 3);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.add(num, 98);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.remainder(num, 2);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.multiply(num, 50);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.remainder(num, 40);
+//     }).then(num => {
+//         console.log(num);
+//         return slowMath.add(num, 32);
+//     }).then(num => {
+//         console.log(num);
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+   
+    
